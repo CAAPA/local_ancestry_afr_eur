17bb5adb2c0efd633ca3721461ab242797ac38d2
Michelle Daya <michelle.daya@ucdenver.edu>
2016-08-01 16:28:30 -0600

    Minor bug fix where correct SNP position columns are now read

--- scripts/get_admixed_snps.py
+++ scripts/get_admixed_snps.py
@@ -20,7 +20,7 @@ ref_file = open("tmp_chr" + chr + "_tgp_mapped.txt")
 snp_map={} #key:snp_pos, val=a1a2
 for line in ref_file:
     e = line.strip().split()
-    key = e[1]
+    key = e[0]
     val = e[2] + e[3]
     snp_map[key] = val
 ref_file.close()
@@ -31,7 +31,7 @@ out_file = open("tmp_chr" + chr + "_snps_keep.txt", "w")
 for line in snp_file:
     e = line.strip().split()
     snp = e[0]
-    key = e[0]
+    key = e[1]
     val = e[2] + e[3]
     val_swapped = e[3] + e[2]
     flipped_val = flip(e[2]) + flip(e[3])
