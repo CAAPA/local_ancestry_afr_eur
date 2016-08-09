00caece442c0afed1bcb79cf13c8c2ac649992ba
Michelle Daya <michelle.daya@gmail>
2016-08-08 20:32:15 -0600

    Fixed script to check for duplicate SNPs

--- scripts/get_ref_haps.py
+++ scripts/get_ref_haps.py
@@ -12,12 +12,14 @@ snp_keep_file.close()
 hap_file = out_file = open("tmp_chr" + chr + "_ref_select.txt")
 out_file = open("tmp_chr" + chr + "_ref.txt", "w")
 hap_file.readline() #skip the header line
+prev_snp = ""
 for line in hap_file:
     e = line.strip().split()
     snp = e[0]
-    if snp in snps_hash.keys():
+    if (snp != prev_snp) and snp in snps_hash.keys():
         for i in range(1, len(e)-1):
             out_file.write(e[i] + " ")
         out_file.write(e[len(e)-1] + "\n")
+    prev_snp = snp
 hap_file.close()
 out_file.close()
